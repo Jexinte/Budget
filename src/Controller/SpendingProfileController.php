3fe64ae6f1db800e24e8cd19b59c9756f80dc27a
Yokke <mdembelepro@gmail.com>
2024-03-21 06:39:23 +0100

    refactor: the homepage configuration is done as the spending profile page

--- src/Controller/SpendingProfileController.php
+++ src/Controller/SpendingProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\SpendingProfile;
 use App\Form\SpendingProfileType;
 use App\Repository\ExpenseRepository;
 use App\Repository\SpendingProfileRepository;
@@ -15,17 +16,23 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class SpendingProfileController extends AbstractController
 {
-    #[Route('/créer-un-profil-de-dépenses', name: 'spendingProfileGet',methods: ['GET'])]
-    public function spendingProfileGet(): Response
+    #[Route('/créer-un-profil-de-dépenses', name: 'createSpendingProfileGet',methods: ['GET'])]
+    public function createSpendingProfileGet(): Response
     {
         $form = $this->createForm(SpendingProfileType::class);
         return $this->render('spending_profile/spending_profile.twig', [
             'form' => $form,
         ]);
     }
-    #[Route('/créer-un-profil-de-dépenses', name: 'spendingProfilePost',methods: ['POST'])]
-    public function spendingProfilePost(Request $request,ExpenseService $expenseService,SpendingProfileRepository $profileRepository,ExpenseRepository $expenseRepository): JsonResponse
+    #[Route('/créer-un-profil-de-dépenses', name: 'createSpendingProfilePost',methods: ['POST'])]
+    public function createSpendingProfilePost(Request $request,ExpenseService $expenseService,SpendingProfileRepository $profileRepository,ExpenseRepository $expenseRepository): JsonResponse
     {
         return !$expenseService->saveProfileAndExpenses($request,$profileRepository,$expenseRepository) ? $this->json(["status" => Response::HTTP_BAD_REQUEST]) : $this->json(["status" => Response::HTTP_OK]);
+    }
+
+    #[Route('/profil-de-dépenses/{slug}',name:'spendingProfileGet',methods:['GET'])]
+    public function spendingProfileGet(SpendingProfile $spendingProfile):Response
+    {
+        dd($spendingProfile);
     }
 }
